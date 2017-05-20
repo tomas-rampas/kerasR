5e8ec4b26e89ee1a39a59e5f00f234d21876492b
taylor <taylor.arnold@acm.org>
2017-05-20 14:23:41 +0200

    run powerscript

--- scripts/appveyor.ps1
+++ scripts/appveyor.ps1
@@ -1,4 +1,21 @@
 Function InstallPythonMods {
-  $cmd = $env:PYTHON + "\Scripts\conda.exe install -c alchayward keras=0.1.2"
+  $cmd = $env:PYTHON + "conda config --set always_yes yes --set changeps1 no"
+  Invoke-Expression $cmd
+
+  $cmd = $env:PYTHON + "conda update -q conda"
+  Invoke-Expression $cmd
+
+  $cmd = $env:PYTHON + "conda info -a"
+  Invoke-Expression $cmd
+
+  $cmd = $env:PYTHON + "conda create -q -n test-environment python=%PYTHON_VERSION% numpy scipy nose"
+  Invoke-Expression $cmd
+
+  $cmd = $env:PYTHON + "activate test-environment"
+  Invoke-Expression $cmd
+
+  $cmd = $env:PYTHON + "pip install keras"
   Invoke-Expression $cmd
 }
+
+
