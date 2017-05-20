c6ad856a66088db20e2b60e38f9bdbd1e839aea9
taylor <taylor.arnold@acm.org>
2017-05-20 13:59:11 +0200

    run powerscript

--- scripts/appveyor.ps1
+++ scripts/appveyor.ps1
@@ -1,8 +1,10 @@
+
+
 Function InstallPythonMods {
   $cmd = "C:\\Python27\python.exe -m pip install numpy"
   Invoke-Expression $cmd
-  $cmd = $env::PYTHON + "\python.exe -m pip install numpy"
+  $cmd = $env:PYTHON + "\python.exe -m pip install numpy"
   Invoke-Expression $cmd
-  $cmd = $env::PYTHON + "\python.exe -m pip install scipy"
+  $cmd = $env:PYTHON + "\python.exe -m pip install scipy"
   Invoke-Expression $cmd
 }
