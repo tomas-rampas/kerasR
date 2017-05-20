e8cb960272a2d011f6749d1473d243c292cefe03
taylor <taylor.arnold@acm.org>
2017-05-20 13:49:39 +0200

    run powerscript

--- scripts/appveyor.ps1
+++ scripts/appveyor.ps1
@@ -1,4 +1,4 @@
 Function InstallPythonMods {
-  $env::PYTHON\\python.exe -m pip install numpy
-  $env::PYTHON\\python.exe -m pip install scipy
+  Exec{ $env::PYTHON\python.exe -m pip install numpy }
+  Exec{ $env::PYTHON\python.exe -m pip install scipy }
 }
